f888a106e3a8675eec67b4e51217bd2a74639064
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 18:10:06 +0700

    Double declaration error

--- lab2_3.cpp
+++ lab2_3.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main()
 {
-	int n[3], s[3], m[3]; // n = number, s = series (ABC), m = most (most 3 to least 1)
+	int n[3], s[3]; // n = number, s = series (ABC)
 	scanf("%d %d %d", &n[0], &n[1], &n[2]);
 	scanf("%c %c %c", &s[0], &s[1], &s[2]);
 	if (n[0] < n[1])
 	{
 		if (n[1] < n[2])
 		{
-			m[3] = {1, 2, 3};
+			int m[3] = {1, 2, 3};  // m = most (most 3 to least 1
 		}
 		else
 		{
-			m[3] = {1, 3, 2};
+			int m[3] = {1, 3, 2};
 		}
 	}
 	else
