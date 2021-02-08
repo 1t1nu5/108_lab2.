e1f06f3acc04b8082507ac2d230b6db93858535c
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 17:57:22 +0700

    More variable declaration & comment.

--- lab2_3.cpp
+++ lab2_3.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main()
 {
-	int n[3], s[3];
+	int n[3], s[3], m[3]; // n = number, s = series (ABC), m = most (most to least)
 	scanf("%d %d %d", &n[0], &n[1], &n[2]);
 	scanf("%c %c %c", &s[0], &s[1], &s[2]);
+	
 	return 0;
 }
