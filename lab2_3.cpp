3fd6379848cf64d6a46a4c07b4e0074635efd27e
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 19:47:51 +0700

    Update lab2_3.cpp

--- lab2_3.cpp
+++ lab2_3.cpp
@@ -8,6 +8,39 @@ int main()
 		scanf("%d", &num[i]);
 	}
 	scanf("%s", order);
+	if (num[0] < num[1])
+	{
+		if (num[1] < num[2])
+		{
+			most[0] = 1;
+			most[1] = 2;
+			most[2] = 3;
+		}
+		else
+		{
+			most[0] = 1;
+			most[1] = 3;
+			most[2] = 2;
+		}
+	}
+	else
+	{
+		if (num[0] < num[2])
+		{
+			most[0] = 2;
+			most[1] = 1;
+			most[2] = 3;
+		}
+		else
+		{
+			most[0] = 2;
+			most[1] = 3;
+			most[2] = 1;
+		}
+	}
+	most[0] = num[most[0]];
+	most[1] = num[most[1]];
+	most[2] = num[most[2]];
 	
 	return 0;
 }
