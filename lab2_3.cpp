3e4adc2bb0e829c4af163769ecb6488a0b0d57c4
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 19:36:16 +0700

    declare and receive variable

--- lab2_3.cpp
+++ lab2_3.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	
+	int num[3], order[3], _order;
+	for (int i = 0; i < 3; i++)
+	{
+		scanf("%d", &num[i]);
+	}
+	scanf("%s", _order);
 	return 0;
 }
