475e0ab87ea8445bbcb432142dfd7c7455654498
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 17:58:32 +0700

    Default value.

--- lab2_3.cpp
+++ lab2_3.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-	int n[3], s[3], m[3]; // n = number, s = series (ABC), m = most (most to least)
+	int n[3], s[3], m[3] = {1, 2, 3}; // n = number, s = series (ABC), m = most (most 3 to least 1)
 	scanf("%d %d %d", &n[0], &n[1], &n[2]);
 	scanf("%c %c %c", &s[0], &s[1], &s[2]);
-	
+	if (n[0] < n[1])
 	return 0;
 }
