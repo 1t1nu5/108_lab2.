3fed6105a4b9dda96c5dd23fbb3707b1b22fcbcc
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 17:59:44 +0700

    No need default value

--- lab2_3.cpp
+++ lab2_3.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main()
 {
-	int n[3], s[3], m[3] = {1, 2, 3}; // n = number, s = series (ABC), m = most (most 3 to least 1)
+	int n[3], s[3], m[3]; // n = number, s = series (ABC), m = most (most 3 to least 1)
 	scanf("%d %d %d", &n[0], &n[1], &n[2]);
 	scanf("%c %c %c", &s[0], &s[1], &s[2]);
 	if (n[0] < n[1])
+	{
+		if (n[1] < n[2]);
+		m[3] = {1, 2, 3};
+	}
 	return 0;
 }
