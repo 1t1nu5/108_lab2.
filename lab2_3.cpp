1cb6baed7b8449d67de4025e6be18133bd60935f
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 18:12:29 +0700

    RIP variable m after }

--- lab2_3.cpp
+++ lab2_3.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
 int main()
 {
-	int n[3], s[3]; // n = number, s = series (ABC)
+	int n[3], s[3], m[3]; // n = number, s = series (ABC), m = most (most 3 to least 1
 	scanf("%d %d %d", &n[0], &n[1], &n[2]);
 	scanf("%c %c %c", &s[0], &s[1], &s[2]);
 	if (n[0] < n[1])
 	{
 		if (n[1] < n[2])
 		{
-			int m[3] = {1, 2, 3};  // m = most (most 3 to least 1
+			m[0] = 1;
+			m[1] = 2;
+			m[3] = 3;
 		}
 		else
 		{
