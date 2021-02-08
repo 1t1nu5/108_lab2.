b7f5209927d06d3e4b9cccb633812a9acd6ba3c8
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 19:00:29 +0700

    reset

--- lab2_2.cpp
+++ lab2_2.cpp
@@ -1,35 +1,6 @@
 #include <stdio.h>
 int main()
 {
-	char choice;
-	float usage, USAGE, cost = 0, overpay;
-	int limit;
-	scanf("%c", &choice);
-	if (choice == 'A')
-	{
-		limit = 12000;
-		overpay = 2;
-		cost = 199;
-	}
-	else if (choice == 'B')
-	{
-		limit = 24000;
-		overpay = 3;
-		cost = 299;
-	}
-	else
-	{
-		return 1;
-	}
-	scanf("%f", &usage);
-	USAGE = (usage/100)*100 * 60;
-	USAGE += (usage - (usage/100)*100 ) * 100;
-	USAGE -= limit;
-	USAGE /= 60;
-	if (USAGE > 0)
-	{
-		cost += overpay*USAGE;
-	}
-	int COST = cost;
-	printf("%d", COST);
+	
+	return 0;
 }
