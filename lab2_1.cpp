78a43da4041bbd55fd5b116a016bc6536d5a1098
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 17:18:09 +0700

    Input Receiving Code Addiction

--- lab2_1.cpp
+++ lab2_1.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	int receive, total;
+	int receive, total = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		scanf("%d", &receive);
+		total += receive;
+	}
 	
 	return 0;
 }
