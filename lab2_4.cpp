8809b4a0c74f0fa633a6cf7727c62c54004d3680
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 18:31:55 +0700

    Lots of codes are changed

--- lab2_4.cpp
+++ lab2_4.cpp
@@ -1,35 +1,37 @@
 #include <stdio.h>
-void fi (int add)
-{
-	if (bday % 5 == 0)
-	{
-		add (add);
-	}
-}
-void add (int add)
-{
-	if (add == 0)
-	{
-		printf("\\");
-	}
-	else
-	{
-		printf("/");)
-	}
-}
 int main()
 {
 	char name;
 	int bday, check;
+	fi (int add)
+	{
+		if (bday % 5 == 0)
+		{
+			if (add == 0)
+			{
+				printf("\\");
+			}
+			else
+			{
+				printf("/");
+			}
+		}
+	}
 	scanf("%c", &name);
 	scanf("%d", &bday);
 	check = (name + bday) % 2;
 	if (name < 'J')
 	{
+		fi(0);
 		if (check == 1)
 		{
-			printf("(^_^)")
+			printf("(^_^)");
 		}
+		else
+		{
+			printf("{@_@}");
+		}
+		fi(1);
 	}
 	return 0;
 }
