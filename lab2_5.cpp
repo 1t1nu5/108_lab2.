ceb76f34d24091ce159b9375878d8fc189654325
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 18:48:41 +0700

    Cases addiction

--- lab2_5.cpp
+++ lab2_5.cpp
@@ -4,5 +4,29 @@ int main()
 	int day, month, result;
 	scanf("%d", &day);
 	scanf("%d", &month);
+	switch (month)
+	{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			month = 31; // variable reusage
+			break;
+		case 2:
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			month = 30;
+			break;
+		case 2:
+			month = 29;
+			break;
+		default:
+			return 1;
+	}
 	return 0;
 }
