36107bf3ed6976814e3ce7577bdf6a9961d7b20c
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 17:23:53 +0700

    Error detection code addiction

--- lab2_1.cpp
+++ lab2_1.cpp
@@ -4,6 +4,21 @@ int main()
 	int receive, total = 0;
 	for (int i = 0; i < 3; i++)
 	{
+		scanf("%d", &receive);
+		if (i == 2)
+		{
+			if (receive < 0 || receive > 40)
+			{
+				return 1;
+			}
+		}
+		else
+		{
+			if (receive < 0 || receive > 30)
+			{
+				return 2;
+			}
+		}
 		total += receive;
 	}
 	if (total >= 80)
