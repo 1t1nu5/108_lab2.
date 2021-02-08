cb49c74d7c0c9ccc9eba4feab3b8e196977859f2
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 19:14:52 +0700

    Try and try

--- lab2_2.cpp
+++ lab2_2.cpp
@@ -5,8 +5,13 @@ int main()
 	float usage;
 	scanf("%c", &promo);
 	scanf("%f", &usage);
-	usage *= 100;
-	int U = usage;
-	printf("%d", U);
+	int U[2];
+	U[0] = (usage/100)*100;
+	U[1] = (usage-U[0])*100;
+	if (U[1] > 60)
+	{
+		return 1;
+	}
+	printf("%d %d", U[0], U[1]);
 	return 0;
 }
