14707946428984f7989f24f09a99758e0b8675bc
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-08 17:21:38 +0700

    Cases addiction

--- lab2_1.cpp
+++ lab2_1.cpp
@@ -4,9 +4,39 @@ int main()
 	int receive, total = 0;
 	for (int i = 0; i < 3; i++)
 	{
-		scanf("%d", &receive);
 		total += receive;
 	}
-	
+	if (total >= 80)
+	{
+		printf("A");
+	}
+	else if (total >= 75)
+	{
+		printf("B+");
+	}
+	else if (total >= 70)
+	{
+		printf("B");
+	}
+	else if (total >= 65)
+	{
+		printf("C+");
+	}
+	else if (total >= 60)
+	{
+		printf("C");
+	}
+	else if (total >= 55)
+	{
+		printf("D+");
+	}
+	else if (total >= 50)
+	{
+		printf("D");
+	}
+	else
+	{
+		printf("F");
+	}
 	return 0;
 }
